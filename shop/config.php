078b5ed6b896b391418de074b365991c9f304108
Brittney Braxton <brittneynbraxton@gmail.com>
2016-08-24 23:00:17 -0500

    Update repo to reflect server details

---
+++ shop/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://thegoodlife.dpechicago.com/thegoodlifeorg/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://thegoodlife.dpechicago.com/thegoodlifeorg/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/catalog/');
+define('DIR_SYSTEM', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/');
+define('DIR_IMAGE', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/image/');
+define('DIR_LANGUAGE', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/config/');
+define('DIR_CACHE', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/storage/download/');
+define('DIR_LOGS', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Hosting/9201621/html/TheGoodLifeOrg/shop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'thegoodlifeorg.db.9201621.hostedresource.com');
+define('DB_USERNAME', 'TheGoodLifeOrg');
+define('DB_PASSWORD', 'TheGoodLife1@');
+define('DB_DATABASE', 'TheGoodLifeOrg');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
